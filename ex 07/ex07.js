4682fae74a0e22b62a2ea1368fe955fc230ba8dd
Ingrid Nogueira <ingrid.silva@al.infnet.edu.br>
2021-10-15 12:17:05 -0300

    ajuste ex07

--- ex 07/ex07.js
+++ ex 07/ex07.js
@@ -1,19 +1,20 @@
 //ex 07
-let horas = new Date().getHours
+let horas = new Date()
 
+horas.setHours(14)
 
-if(horas.getHours >= 6 && horas.getHours < 12){
+if(horas.getHours() >= 6 && horas.getHours() < 12){
 
-  console.log(horas.getHours)
+  console.log(horas.getHours())
   console.log("Buenos dias")
 
-}else if(horas.getHours >= 12 && horas.getHours < 18){
+}else if(horas.getHours() >= 12 && horas.getHours() < 18){
 
-  console.log(horas.getHours)
+  console.log(horas.getHours())
   console.log("Buenas tardes")
 
 }else{
 
-  console.log(horas.getHours)
+  console.log(horas.getHours())
   console.log("Buenas noches")
 }
