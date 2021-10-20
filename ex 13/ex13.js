bade8b565550ad83cb12eaa33a737766e08f8ce3
Ingrid Nogueira <ingrid.silva@al.infnet.edu.br>
2021-10-20 14:22:08 -0300

    Adiciona ex 13

---
+++ ex 13/ex13.js
@@ -0,0 +1,11 @@
+let numeroInicial = parseInt(prompt("Informe o número inicial"))
+let numeroFinal = parseInt(prompt("Informe o número final"))
+let contador = numeroFinal - numeroInicial
+
+while (numeroInicial < contador){
+
+    numeroInicial += 1
+    console.log(numeroInicial)
+
+}
+
